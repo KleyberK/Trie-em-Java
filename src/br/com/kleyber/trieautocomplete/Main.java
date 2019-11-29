070d385da5a7778ea4b72c63aa266f48221e6dc1
Kleyber José <kleyber2006@hotmail.com>
2019-11-29 00:32:57 -0300

    Adicionado Métodos de Inserção e Busca de palavras

--- src/br/com/kleyber/trieautocomplete/Main.java
+++ src/br/com/kleyber/trieautocomplete/Main.java
@@ -8,26 +8,58 @@ public class Main {
     public static void main(String[] args) {
         TrieNode trie = new TrieNode(' ');
         Scanner leitor = new Scanner(System.in);
-        String palavra;
-        for(int i = 0; i < 2; i++){
-            System.out.print("Digite uma palavra: ");
-            palavra = leitor.next();
-            adicionaPalavra(trie, palavra);
-        }
-        ArrayList<TrieNode> Children = new ArrayList<>(trie.getChildren());
-        for(int i = 0; i < Children.size(); i++){
-            System.out.println(Children.get(i).getName());
+        String escolha = "0";
+
+        while(!escolha.equalsIgnoreCase("05")){
+            System.out.println("Escolha uma das seguintes opções:");
+            System.out.println("      01 - Cadastrar palavra\n      02 - Buscar palavra\n      03 - Remover palavra" +
+                    "\n      04 - Autocompletar da palavra\n      05 - Sair do programa");
+            System.out.print("Opção: ");
+            escolha = leitor.next();
+
+            if (escolha.equalsIgnoreCase("01")){
+                adicionaPalavra(trie);
+            } else if (escolha.equalsIgnoreCase("02")){
+                buscaPalavra(trie);
+            } else if(escolha.equalsIgnoreCase("03")){
+
+            } else if (escolha.equalsIgnoreCase("04")){
+
+            }
         }
     }
 
-    public static void adicionaPalavra(TrieNode trie, String palavra){
-        for(int i = 0; i < palavra.length(); i++){
-            char p = palavra.charAt(i);
-            if(i == palavra.length() -1){
-                trie.addChildWord(p);
-            } else {
-                trie.addChild(p);
+    public static void adicionaPalavra(TrieNode trie){
+        Scanner leitor1 = new Scanner(System.in);
+        String palavra;
+        System.out.print("Digite a palavra: ");
+        palavra = leitor1.next();
+        trie.addChild(palavra);
+    }
+
+    public static void buscaPalavra(TrieNode trie){
+        Scanner leitor2 = new Scanner(System.in);
+        String palavraBusca;
+        System.out.print("Digite a palavra: ");
+        palavraBusca = leitor2.next();
+        TrieNode achou = new TrieNode();
+        String palavraFinal = new String();
+        int i = 0;
+        while(i < palavraBusca.length()){
+            achou = trie.busca(trie, palavraBusca.charAt(i));
+            if(achou != null){
+                if(!achou.isWord){
+                    palavraFinal += achou.getName();
+                } else{
+                    palavraFinal += achou.getName();
+                }
             }
+            i++;
+        }
+        if(palavraFinal.equalsIgnoreCase(palavraBusca)){
+            System.out.println("Palavra Encontrada");
+        } else {
+            System.out.println("Palavra não encontrada");
         }
     }
 }
--- src/br/com/kleyber/trieautocomplete/trie/TrieNode.java
+++ src/br/com/kleyber/trieautocomplete/trie/TrieNode.java
@@ -3,12 +3,15 @@ package br.com.kleyber.trieautocomplete.trie;
 import java.util.ArrayList;
 
 public class TrieNode {
-    boolean isWord;
+    public boolean isWord;
     String Text;
-
-    private ArrayList<TrieNode> children;
+    public ArrayList<TrieNode> children;
     private char name;
     private TrieNode parent = null;
+
+    public TrieNode(){
+        this.isWord = false;
+    }
 
     public TrieNode(char name) {
         this.name = name;
@@ -16,19 +19,30 @@ public class TrieNode {
     }
 
 
-    public TrieNode addChild(char s) {
-        TrieNode node = new TrieNode(s);
-        return addChild(node);
+    public void addChild(String palavra) {
+        for(int i = 0; i < palavra.length(); i++){
+            char p = palavra.charAt(i);
+            int j = 0;
+            while(j<children.size()){
+                if(children.get(j).getName() == p){
+                    i++;
+                    TrieNode nodeFilho = new TrieNode(palavra.charAt(i));
+                    nodeFilho.parent = this;
+                    addChild(nodeFilho);
+                }
+                j++;
+            }
+            if(i == palavra.length() -1){
+                this.addChildWord(p);
+            } else {
+                TrieNode node = new TrieNode(p);
+                addChild(node);
+            }
+
+        }
     }
 
     public TrieNode addChild(TrieNode node) {
-        int i = 0;
-        while(i<children.size()){
-            if(children.get(i).getName() == node.getName()){
-                return node;
-            }
-            i++;
-        }
         children.add(node);
         node.parent = this;
         return node;
@@ -50,6 +64,24 @@ public class TrieNode {
 
     public void setName(char name) {
         this.name = name;
+    }
+
+    public TrieNode busca(TrieNode pai, char procurado) {
+        if (procurado == name) {
+            return this;
+        }
+        for (TrieNode filho : pai.children) {
+            TrieNode achou = filho.busca(procurado);
+            if (achou != null) {
+                return achou;
+            } else{
+                TrieNode achou2 = filho.busca(filho, procurado);
+                if (achou2 != null){
+                    return achou2;
+                }
+            }
+        }
+        return null;
     }
 
     public TrieNode busca(char procurado) {
